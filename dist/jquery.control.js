352151bacd48cbc3599ee468d0eb23bff43f205c
Stepan Kosatyi <stepan@kosatyi.com>
2023-02-27 23:01:36 +0200

    update builder

--- dist/jquery.control.js
+++ dist/jquery.control.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.jQueryControl = factory());
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, (global.jquery = global.jquery || {}, global.jquery.control = factory()));
 })(this, (function () { 'use strict';
 
     const classes$2 = {};
