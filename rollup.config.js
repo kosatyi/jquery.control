b5b713d28505b08f12ccf775597db18c7d8b6309
Stepan Kosatyi <stepan@kosatyi.com>
2023-02-27 19:40:29 +0200

    update

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import {resolve, commonjs, babel, terser} from '@kosatyi/rollup'
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+const terserConfig = {
+    mangle: {
+        reserved: ['$'],
+    },
+    format: {
+        comments: false,
+    },
+}
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: 'dist/jquery.control.js',
+            format: 'umd',
+            name: '$',
+            globals: {
+                jquery: '$',
+            },
+        },
+        {
+            file: 'dist/jquery.control.min.js',
+            sourcemap: true,
+            format: 'umd',
+            name: '$',
+            globals: {
+                jquery: '$',
+            },
+            plugins: [
+                sourcemaps(),
+                terser(terserConfig),
+            ]
+        }
+    ],
+    external: ['jquery'],
+    plugins: [
+        commonjs(),
+        resolve(),
+        babel({
+            babelHelpers: 'bundled',
+        }),
+    ],
+}
