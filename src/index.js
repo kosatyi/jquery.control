b6b3ef5263833f9d7782fcec4ac3c0547bf91afd
Stepan Kosatyi <stepan@kosatyi.com>
2023-12-07 14:48:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,98 +8,28 @@ import {Router} from './core/router'
 import {view} from './core/view'
 import {Cache} from './utils/cache'
 
-/**
- * @memberOf $
- * @property storageCache
- */
 $.storageCache = Cache
-/**
- * @memberOf $
- * @property Class
- */
 $.Class = Class;
-/**
- * @memberOf $
- * @property Model
- */
 $.Model = Model;
-/**
- * @memberOf $
- * @property Control
- */
 $.Control = Control;
-/**
- * @memberOf $
- * @property Router
- */
 $.Router = Router;
-/**
- * @memberOf $
- * @property createClass
- * @type {function(*=, *, *): (*)}
- */
 $.createClass = createClass;
-/**
- * @memberOf $
- * @property getClass
- * @type {function(*, *=): (undefined|*)}
- */
 $.getClass = getClass;
-/**
- * @memberOf $
- * @property createModel
- * @type {function(*=, *, *): (*)}
- */
 $.createModel = createModel;
-/**
- * @memberOf $
- * @property getModel
- * @type {function(*, *=): (undefined|*)}
- */
 $.getModel = getModel;
-/**
- *
- * @type {function(*=, *, *): *}
- */
 $.createControl = createControl;
-/**
- * @memberOf $
- * @property initControl
- * @type {function(*, *=): (undefined|*)}
- */
 $.initControl = initControl;
-/**
- * @memberOf $
- * @property initControl
- * @type {function(*, *=): (undefined|*)}
- */
 $.cleanControls = cleanControls;
-/**
- * @memberOf $
- * @property location
- * @type {Object}
- */
 $.location = urlLocation;
-/**
- * @memberOf $
- */
 $.deparam = deparam
-/**
- * @memberOf $
- * @property ejs
- * @deprecated
- */
 $.ejs = view;
-
-$.fn.extend({
-    setFormData: setFormData,
-    getFormData: getFormData,
-    initControls: function () {
-        this.each(function (index, element) {
-            initControls(element);
-        });
-    }
-});
+$.fn.setFormData = setFormData
+$.fn.getFormData = getFormData
+$.fn.initControls = function () {
+    this.each(function (index, element) {
+        initControls(element);
+    });
+}
 
 export {
     $,
