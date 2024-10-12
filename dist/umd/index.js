a69ce721ba4e0298dc7e938d84f6e43a2b6c499d
Stepan Kosatyi <stepan@kosatyi.com>
2024-10-12 14:46:45 +0300

    update control

--- dist/umd/index.js
+++ dist/umd/index.js
@@ -1044,6 +1044,12 @@
         this._event_('off', arguments);
         return this;
       },
+      trigger(name) {
+        const params = this.toArray(arguments).slice(1);
+        const event = jQuery.Event(name);
+        this.element.trigger.apply(this.element, [event].concat(params));
+        return event;
+      },
       delay(callback, time) {
         callback = this.proxy(callback, true);
         const params = [callback, time].concat(Array.from(arguments).slice(2));
